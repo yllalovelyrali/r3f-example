81b87f488ca4ac918dac33b3c051c8c85cf97e0d
YIYE Li <yllalove.lyra@gmail.com>
2022-08-22 18:35:29 +0800

    add clouds

--- src/globe/index.jsx
+++ src/globe/index.jsx
@@ -1,14 +1,37 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useTexture } from '@react-three/drei'
+import { MeshBasicMaterial, MeshPhongMaterial } from 'three'
 
 
 export default function Expenses() {
+
+  function Globe(){
+    const [matcap1] = useTexture(['earthmap10k.jpg'])
+    return<mesh name='earth'>
+    <sphereGeometry args={[4, 30, 30]} />
+    {/* <meshNormalMaterial wireframe/> */}
+    <meshPhongMaterial map={matcap1} />
+    <sphereGeometry args={[4.01, 20, 20]} />
+    </mesh>
+  }
+
+  function Clouds(){
+    const [cloudTexture] = useTexture(['fair_clouds_4k.png'])
+    return <mesh name='earth'>
+    <sphereGeometry args={[4.05, 30, 30]} />
+    {/* <meshNormalMaterial wireframe/> */}
+    <meshBasicMaterial
+      map={cloudTexture}
+      transparent={true}
+    />
+    </mesh>
+  }
+
   return (
     <Canvas style={{height:'50em'}}>
-      <mesh name='earth'>
-      <sphereGeometry args={[4, 20, 20]} />
-      <meshNormalMaterial wireframe/>
-      </mesh>
+      <Globe />
+      <Clouds />
+      <ambientLight color="0x111111" />
       <OrbitControls />
     </Canvas>
   );
