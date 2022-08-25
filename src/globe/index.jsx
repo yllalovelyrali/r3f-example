c302fa97bb44e89b4d7b7f9c56b81df2c1bd84bd
YIYE Li <yllalove.lyra@gmail.com>
2022-08-25 12:01:13 +0800

    the sky box

--- src/globe/index.jsx
+++ src/globe/index.jsx
@@ -1,24 +1,28 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useTexture } from '@react-three/drei'
+import { Canvas, useThree, useFrame, extend } from '@react-three/fiber'
+import { OrbitControls, useTexture, Plane } from '@react-three/drei'
 import { MeshBasicMaterial, MeshPhongMaterial } from 'three'
+import {useRef, useEffect} from 'react'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
 
+extend({ EffectComposer, RenderPass, GlitchPass })
 
 export default function Expenses() {
 
   function Globe(){
     const [matcap1] = useTexture(['earthmap10k.jpg'])
     return<mesh name='earth'>
-    <sphereGeometry args={[4, 30, 30]} />
+    <sphereGeometry args={[1, 30, 30]} />
     {/* <meshNormalMaterial wireframe/> */}
     <meshPhongMaterial map={matcap1} />
-    <sphereGeometry args={[4.01, 20, 20]} />
     </mesh>
   }
 
   function Clouds(){
     const [cloudTexture] = useTexture(['fair_clouds_4k.png'])
     return <mesh name='earth'>
-    <sphereGeometry args={[4.05, 30, 30]} />
+    <sphereGeometry args={[1.03, 30, 30]} />
     {/* <meshNormalMaterial wireframe/> */}
     <meshBasicMaterial
       map={cloudTexture}
@@ -27,12 +31,52 @@ export default function Expenses() {
     </mesh>
   }
 
+  function Background(){
+    const [bgTexture] = useTexture(['starry_background.jpg'])
+
+    return <Plane
+      args={[1, 1]}
+      scale={[window.innerWidth * 2, window.innerHeight * 2, 1]}
+      position={[0,0,-100]}
+    >
+      <meshBasicMaterial map={bgTexture} />
+    </Plane>
+  }
+
+  function CustomiseCamera(){
+    return <orthographicCamera
+      left={-window.innerWidth}
+      right={window.innerWidth}
+      top={-window.innerHeight}
+      bottom={window.innerHeight}
+      near={-10000}
+      far={10000}
+      position={[0,0,50]}
+    />
+  }
+
+  function Effect(){
+    const { scene, gl, size, camera } = useThree();
+    // const composer = useRef()
+    // useEffect(() => void composer.current.setSize(size.width, size.height), [size])
+    // useFrame(() => composer.current.render(), 1)
+    return(
+      <effectComposer args={[gl]}>
+        <renderPass args={[scene, camera]} />
+        <glitchPass renderToScreen />
+      </effectComposer>
+    )
+  }
+
   return (
-    <Canvas style={{height:'50em'}}>
+    <Canvas style={{height:'50em'}} camera={CustomiseCamera}>
       <Globe />
       <Clouds />
       <ambientLight color="0x111111" />
-      <OrbitControls />
+      <directionalLight color="0xffffff" position={[100, 10, -50]}/>
+      {/* <OrbitControls /> */}
+      <Effect />
+      <Background />
     </Canvas>
   );
 }
